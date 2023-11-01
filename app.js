ca73128a1d9bb2a18ee5433a8eab4e55bc81f100
Larisa Ornela Picco <seleneneptunotesalia@gmail.com>
2023-11-01 21:58:20 +0100

    actualizacion de js, formulario

--- app.js
+++ app.js
@@ -10,3 +10,30 @@ window.onscroll = () =>{  /*onscroll call a function when a <div> element is bei
     menu.classList.remove('fa-times');
     nav.classList.remove('active')
 }
+
+const nombre = document.getElementById('name')
+const email = document.getElementById('email')
+const form = document.getElementById('form')
+const parrafo = document.getElementById('warnings')
+
+form.addEventListener('submit', e => {
+    e.preventDefault()
+    let warnings = ""
+    let entrar = false
+    let regexEmail = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/ /*texto regular por defecto que sirve para validar un email */
+    parrafo.innerHTML = ""
+    if(nombre.value.length < 6){
+       warnings+= `Escribe un nombre con un largo mayor a 6 caracteres <br>`
+       entrar = true
+    }
+    if(!regexEmail.test(email.value)){
+        warnings+= `El email no es válido <br>`
+        entrar = true
+    }
+
+    if(entrar){
+        parrafo.innerHTML = warnings
+    } else{
+        parrafo.innerHTML = "Enviado"
+    }
+})
